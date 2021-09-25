20a8eb3a332f25e482e249d20339b019ad9e486a
Vedanta Bhargava <72670340+codingwithvb@users.noreply.github.com>
2021-09-25 11:28:58 -0400

    Add files via upload

---
+++ CollectTheFactors.py
@@ -0,0 +1,24 @@
+number = input("What is the Number?")
+number_value = int(number)
+
+number1 = number_value % 1
+
+if number1 == 0:
+  print("1")
+
+number2 = number_value % 2
+
+if number2 == 0:
+  print("2")
+
+number3 = number_value % 3
+
+if number3 == 0:
+  print("3")
+
+number4 = number_value % 4
+
+if number4 == 0:
+  print("4")
+
+
